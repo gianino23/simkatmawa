e543e63132206dbee150f4d29c991dba3c6a7d60
F <60839764+fjrn-azwry@users.noreply.github.com>
2021-10-14 19:16:20 +0800

    Konsep Beranda

--- protected/config/main.php
+++ protected/config/main.php
@@ -7,7 +7,7 @@
 // CWebApplication properties can be configured here.
 return array(
 	'basePath'=>dirname(__FILE__).DIRECTORY_SEPARATOR.'..',
-	'name'=>'Simkatmawa',
+	'name'=>'SIMKIMAHAL',
 	'language'=>'id',
 
 	// preloading 'log' component
