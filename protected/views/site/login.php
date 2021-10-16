169c769099062dc519f92c5cc14c6e7b7c97f3df
gianino <rian2g@gmail.com>
2021-10-16 09:01:54 +0800

    deploy to simkatmawa.borneomediateknologi.co.id
    deploy to cpanel

--- protected/views/site/login.php
+++ protected/views/site/login.php
@@ -1,16 +1,15 @@
 <div id="loginform" class="mt-5">
     <div class="logo text-center">
         <span class="db"><img
-                src="<?php echo Yii::app()->request->baseUrl; ?>/images/ulm.png" height="100px"
+                src="<? echo Yii::app()->request->baseUrl; ?>/images/ulm.png" height="100px"
                 alt="logo" /><br />
-            <!-- <img src="<?php// echo Yii::app()->request->baseUrl; ?>/themes/materialpro/material-pro/src/assets/images/logo-text.png"
-                alt="Home" /></span> -->
+           
                 <h3 class="mt-2">SIMKIMAHAL</h3>
     </div>
     <!-- Form -->
     <div class="row">
         <div class="col-12">
-            <?php $form=$this->beginWidget('CActiveForm', array(
+            <? $form=$this->beginWidget('CActiveForm', array(
                 'id'=>'login-form',
                 'enableClientValidation'=>true,
                 'clientOptions'=>array(
@@ -22,31 +21,31 @@
             )); ?>
             <div class="form-group mb-3">
                 <div class="col-xs-12">
-                    <?php echo $form->textField($model, 'username', ['class' => "form-control", 'placeholder' => "Username", 'required'=>true ]); ?>
+                    <? echo $form->textField($model, 'username', ['class' => "form-control", 'placeholder' => "Username", 'required'=>true ]); ?>
 
                     <label class="error jquery-validation-error small form-text invalid-feedback"
                         for="validation-checkbox-custom-group-1"
-                        style="display: inline-block;"><strong><?php echo $form->error($model, 'username',['style'=>'text-align:center']); ?></strong></label>
+                        style="display: inline-block;"><strong><? echo $form->error($model, 'username',['style'=>'text-align:center']); ?></strong></label>
                 </div>
                 <div class="form-group mb-3">
                     <div class="col-xs-12">
-                        <?php echo $form->passwordField($model, 'password', ['class' => "form-control", 'placeholder' => 'Password', 'required'=>true]); ?>
+                        <? echo $form->passwordField($model, 'password', ['class' => "form-control", 'placeholder' => 'Password', 'required'=>true]); ?>
                         <label class="error jquery-validation-error small form-text invalid-feedback"
                             for="validation-checkbox-custom-group-1"
-                            style="display: inline-block;"><strong><?php echo $form->error($model, 'password',['style'=>'text-align:center']); ?></strong></label>
+                            style="display: inline-block;"><strong><? echo $form->error($model, 'password',['style'=>'text-align:center']); ?></strong></label>
                     </div>
         </div>
                     <div class="form-group text-center mt-3">
                         <div class="col-xs-12">
-                            <a href="<?php echo Yii::app()->request->baseUrl; ?>"
+                            <a href="<? echo Yii::app()->request->baseUrl; ?>"
                                 class="btn btn-danger col-xs-6 col-md-4" style="text-decoration:none;">Cancel</a>
-                            <?php // echo CHtml::Button('Cancel',array('onClick'=>"location='index.php'",'class'=>'text-danger')); ?>
-                            <?php echo CHtml::submitButton('Login', ['class' => 'btn btn-primary col-xs-6 col-md-4']);?>
+                            <? // echo CHtml::Button('Cancel',array('onClick'=>"location='index.php'",'class'=>'text-danger')); ?>
+                            <? echo CHtml::submitButton('Login', ['class' => 'btn btn-primary col-xs-6 col-md-4']);?>
 
                         </div>
                     </div>
 
-                    <?php $this->endWidget(); ?>
+                    <? $this->endWidget(); ?>
                     <center>
                     <p class="text-center" style="bottom:0;width:100%;text-decoration:none;">Ulahan
                         buhan <a href="https://borneomediateknologi.co.id/" target="_blank" class="text-primary">Borneo
