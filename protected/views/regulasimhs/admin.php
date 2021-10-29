0649dc8d655a98042fc882fb3353461350557b72
gianino <rian2g@gmail.com>
2021-10-30 06:56:14 +0800

    Tombol Download Regulasimhs
    Menambahkan Tombol Download Regulasimhs

--- protected/views/regulasimhs/admin.php
+++ protected/views/regulasimhs/admin.php
@@ -85,7 +85,7 @@ $(document).ready(function() {
 										<td><?= $p->isi; ?></td>
 										<td><?= $p->status; ?></td>
 										<td align="center">
-											
+											<a href="upload/<?php echo $p->isi;?>" onclick="return confirm('Apakah Anda ingin mendownload file ini ?');"><i class="fas fa-download"></i></a>
 											<a href="javascript:void(0)" class="delete_modal" data-id='<?php echo  $p->id_regulasimhs; ?>'><i class="fas fa-trash-alt"></i></a>
 										</td>
 									</tr>
