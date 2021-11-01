ecbc81e008e2bf2c508e5840eedac9f15f0862fd
gianino <rian2g@gmail.com>
2021-11-01 20:06:10 +0800

    Update Summernote
    Update summornote untuk upload gambar

--- protected/controllers/ProfilController.php
+++ protected/controllers/ProfilController.php
@@ -28,7 +28,7 @@ class ProfilController extends Controller
 	{
 		return array(
 			array('allow',  // allow all users to perform 'index' and 'view' actions
-				'actions'=>array('index','view','adminn','update','detail','tentang','strukturorganisasi'),
+				'actions'=>array('index','view','adminn','update','detail','tentang','strukturorganisasi','upload'),
 				'users'=>array('*'),
 			),
 			array('allow', // allow authenticated user to perform 'create' and 'update' actions
@@ -54,6 +54,23 @@ class ProfilController extends Controller
 		$this->render('view',array(
 			'model'=>$this->loadModel($id),
 		));
+	}
+	public function actionUpload()
+	{
+	if ($_FILES['file']['name']) {
+  if (!$_FILES['file']['error']) {
+    $name = md5(rand(100, 200));
+    $ext = pathinfo($_FILES['file']['name'], PATHINFO_EXTENSION);
+    $filename = $name.
+    '.'.$ext;
+    $destination = '/uploads/'.$filename; //change this directory
+    $location = $_FILES["file"]["tmp_name"];
+    move_uploaded_file($location, $destination);
+    echo '/uploads/'.$filename; //change this URL
+  } else {
+    echo $message = 'Ooops!  Your upload triggered the following error:  '.$_FILES['file']['error'];
+  }
+}
 	}
 	public function actionAdminn()
 	{
@@ -89,10 +106,55 @@ class ProfilController extends Controller
 	 */
 	public function actionTambah()
 	{
+		$temp = "upload/profil";
+		
+		if (!file_exists($temp))
+			mkdir($temp);
+		
 		$model=new Profil;
 
 		$model->jenis_id = $_POST['modal_nama'];
-		$model->isi = $_POST['modal_isi'];
+		$submitted_text=$_POST['modal_isi'];
+		if (strpos($submitted_text, '<img') !== false && strpos($submitted_text, ';base64') !== false) {
+
+    $doc = new DOMDocument();
+    $doc->loadHTML($submitted_text);
+
+    $tags = $doc->getElementsByTagName('img');
+
+    foreach ($tags as $tag) {
+        // Get base64 encoded string
+        $srcStr = $tag->getAttribute('src');
+        $base64EncData = substr($srcStr, ($pos = strpos($srcStr, 'base64,')) !== false ? $pos + 7 : 0);
+        $base64EncData = substr($base64EncData, 0, -1);
+
+        // Get an image file
+        $img = base64_decode($base64EncData);
+
+        // Get file type
+        $dataInfo = explode(";", $srcStr)[0];
+        $fileExt = str_replace('data:image/', '', $dataInfo);
+
+        // Create a new filename for the image
+        $newImageName = str_replace(".", "", uniqid("forum_img_", true));
+        $filename = $newImageName . '.' . $fileExt;
+        $file = 'upload/profil/' . $filename;
+
+        // Save the image to disk
+        $success = file_put_contents($file, $img);
+        $imgUrl = 'upload/profil/' . $filename;
+
+        // Update the forum thread text with an img tag for the new image
+        $newImgTag = '<img src="' . $imgUrl . '" />';
+
+        $tag->setAttribute('src', $imgUrl);
+        $tag->setAttribute('data-original-filename', $tag->getAttribute('data-filename'));
+        $tag->removeAttribute('data-filename');
+        $submitted_text = $doc->saveHTML();
+		
+    }
+}
+		$model->isi = $submitted_text;
 		$model->periode=$_POST['modal_periode'];
 		$model->save();
 		
@@ -121,9 +183,54 @@ class ProfilController extends Controller
 	 */
 	public function actionEdit()
 	{
+		$temp = "upload/profil";
+		
+		if (!file_exists($temp))
+			mkdir($temp);
+		
 		$modal_idprofil=$_POST['modal_idprofil'];
 		$model=Profil::model()->findByAttributes(array('id_profil'=>$modal_idprofil));
-		$model->isi=$_POST['modal_isi'];
+		$submitted_text=$_POST['modal_isi'];
+		if (strpos($submitted_text, '<img') !== false && strpos($submitted_text, ';base64') !== false) {
+
+    $doc = new DOMDocument();
+    $doc->loadHTML($submitted_text);
+
+    $tags = $doc->getElementsByTagName('img');
+
+    foreach ($tags as $tag) {
+        // Get base64 encoded string
+        $srcStr = $tag->getAttribute('src');
+        $base64EncData = substr($srcStr, ($pos = strpos($srcStr, 'base64,')) !== false ? $pos + 7 : 0);
+        $base64EncData = substr($base64EncData, 0, -1);
+
+        // Get an image file
+        $img = base64_decode($base64EncData);
+
+        // Get file type
+        $dataInfo = explode(";", $srcStr)[0];
+        $fileExt = str_replace('data:image/', '', $dataInfo);
+
+        // Create a new filename for the image
+        $newImageName = str_replace(".", "", uniqid("", true));
+        $filename = $newImageName . '.' . $fileExt;
+        $file = 'upload/profil/'.$filename;
+
+        // Save the image to disk
+        $success = file_put_contents($file, $img);
+        $imgUrl = 'upload/profil/' . $filename;
+
+        // Update the forum thread text with an img tag for the new image
+        $newImgTag = '<img src="' . $imgUrl . '" />';
+
+        $tag->setAttribute('src', $imgUrl);
+        $tag->setAttribute('data-original-filename', $tag->getAttribute('data-filename'));
+        $tag->removeAttribute('data-filename');
+        $submitted_text = $doc->saveHTML();
+		
+    }
+}
+		$model->isi = $submitted_text;
 		$model->periode=$_POST['modal_periode'];
 		
 		if($model->save())
